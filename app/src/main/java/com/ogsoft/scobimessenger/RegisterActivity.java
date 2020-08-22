6bee06f6c8cf0bca43b13ac61ea3844896136044
Onur Osman Güler <guleronurosman@gmail.com>
2020-08-22 20:33:52 +0300

    Register api request

--- app/src/main/java/com/ogsoft/scobimessenger/RegisterActivity.java
+++ app/src/main/java/com/ogsoft/scobimessenger/RegisterActivity.java
@@ -1,6 +1,5 @@
 package com.ogsoft.scobimessenger;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -17,8 +16,6 @@ import android.widget.Toast;
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.error.ANError;
 import com.androidnetworking.interfaces.JSONObjectRequestListener;
-import com.google.gson.Gson;
-import com.google.gson.JsonParser;
 import com.ogsoft.scobimessenger.models.User;
 import com.ogsoft.scobimessenger.services.APIEndpoints;
 import com.ogsoft.scobimessenger.services.Tools;
@@ -26,9 +23,9 @@ import com.ogsoft.scobimessenger.services.Tools;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
-    private EditText et_email, et_password;
+    private EditText et_name, et_username, et_email, et_password, et_password_confirm;
     private TextView txt_error;
 
     private ProgressDialog pd;
@@ -36,47 +33,60 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
+        et_name = findViewById(R.id.et_name);
+        et_username = findViewById(R.id.et_username);
         et_email = findViewById(R.id.et_email);
         et_password = findViewById(R.id.et_password);
+        et_password_confirm = findViewById(R.id.et_password_confirm);
         txt_error = findViewById(R.id.txt_error);
     }
 
-    public void onClickLogin(View view) {
+    public void onClickSignUp(View view) {
         txt_error.setVisibility(View.GONE);
 
+        String name = et_name.getText().toString().trim();
+        String username = et_username.getText().toString().trim();
         String email = et_email.getText().toString().trim();
         String password = et_password.getText().toString();
+        String password_confirm = et_password_confirm.getText().toString();
 
         if (!Tools.isNetworkAvailable(this)) {
             Toast.makeText(this, "Please check your internet connection.", Toast.LENGTH_SHORT).show();
+        } else if (name.isEmpty()) {
+            et_name.setError("Name cannot be empty");
+        } else if (username.isEmpty()) {
+            et_username.setError("Username cannot be empty");
         } else if (email.isEmpty()) {
             et_email.setError("Email cannot be empty");
         } else if (password.isEmpty()) {
             et_password.setError("Password cannot be empty");
+        } else if (!password.equals(password_confirm)) {
+            et_password.setError("Passwords does not match");
         } else {
             pd = new ProgressDialog(this);
-            pd.setMessage("Loading...");
+            pd.setTitle("Loading...");
             pd.setCancelable(false);
             pd.show();
 
             // Set the request body
-            JSONObject jsonObject = new JSONObject();
+            JSONObject body = new JSONObject();
 
             try {
-                jsonObject.put("email", email);
-                jsonObject.put("password", password);
+                body.put("name", name);
+                body.put("username", username);
+                body.put("email", email);
+                body.put("password", password);
             } catch (JSONException e) {
                 e.printStackTrace();
                 txt_error.setText("Uncaughted error. Please try again.");
                 txt_error.setVisibility(View.VISIBLE);
-                return;
             }
 
-            // Send post request
-            AndroidNetworking.post(APIEndpoints.signIn)
-                    .addJSONObjectBody(jsonObject)
+            // Send post request to signup endpoint
+            AndroidNetworking.post(APIEndpoints.signUp)
+                    .addJSONObjectBody(body)
                     .build()
                     .getAsJSONObject(new JSONObjectRequestListener() {
                         @Override
@@ -112,8 +122,7 @@ public class LoginActivity extends AppCompatActivity {
                                 }
                                 editor.commit();
 
-                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                                startActivity(intent);
+                                setResult(Activity.RESULT_OK);
                                 finish();
                             } catch (JSONException e) {
                                 e.printStackTrace();
@@ -140,19 +149,14 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-    public void onClickSignUp(View view) {
-        Intent intent = new Intent(this, RegisterActivity.class);
-        startActivityForResult(intent, 100);
+    public void onClickLogin(View view) {
+        setResult(Activity.RESULT_CANCELED);
+        finish();
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == 100 && resultCode == Activity.RESULT_OK) {
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(intent);
-            finish();
-        }
+    public void onBackPressed() {
+        setResult(Activity.RESULT_CANCELED);
+        super.onBackPressed();
     }
 }
---
+++ app/src/main/java/com/ogsoft/scobimessenger/services/APIEndpoints.java
@@ -0,0 +1,11 @@
+package com.ogsoft.scobimessenger.services;
+
+public class APIEndpoints {
+
+    public static final String BASE_URL = "http://192.168.1.106:3000";
+
+    public static final String signIn = BASE_URL + "/api/v1/auth/signin";
+
+    public static final String signUp = BASE_URL + "/api/v1/auth/signup";
+
+}
